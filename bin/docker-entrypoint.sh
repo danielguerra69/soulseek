c5c9d58ac35bb2962efedf73c06657d4616f849f
danielguerra69 <daniel.guerra69@gmail.com>
2019-01-24 13:08:50 +0100

    typo -r = -R

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo soulseek | sudo -S chown -r soulseek:soulseek /home/soulseek
-echo soulseek | sudo -S chmod -r a+r /home/soulseek
+echo soulseek | sudo -S chown -R soulseek:soulseek /home/soulseek
+echo soulseek | sudo -S chmod -R a+r /home/soulseek
 exec "$@"
